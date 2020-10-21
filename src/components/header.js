e9b1f1fdcca43ed810334f308feabba81f088d2a
moritzsalla <moritz.salla@hotmail.de>
2020-10-21 19:38:41 +0100

    Added Link

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 // ⑲
 
 const Header = () => (
   <header className='mb-2 mb-md-3'>
-    <h2>
-      MSc Creative Computing Graduates ⓳
-      <br />
-      UAL Creative Computing Institute
-    </h2>
+    <Link to='/'>
+      <h2>
+        MSc Creative Computing Graduates ⓳
+        <br />
+        UAL Creative Computing Institute
+      </h2>
+    </Link>
   </header>
 );
 
