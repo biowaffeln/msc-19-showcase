12a44c5e176cb9afcf072bbc01e6151c78d819df
moritzsalla <moritz.salla@hotmail.de>
2020-10-21 15:02:04 +0100

    added icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,13 +62,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ual-cci-graduates-19`,
+        short_name: `cci-19`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `white`,
+        theme_color: `white`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
