4831d82b23ee4a57b5ce80cc4423bd3f5e115837
moritzsalla <moritz.salla@hotmail.de>
2020-10-21 11:25:14 +0100

    hotfix 404

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/moritzsalla/Documents/GitHub/msc-19-showcase/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/moritzsalla/Documents/GitHub/msc-19-showcase/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/moritzsalla/Documents/GitHub/msc-19-showcase/src/pages/index.js"))),
   "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/moritzsalla/Documents/GitHub/msc-19-showcase/src/templates/blog-post.js")))
 }
